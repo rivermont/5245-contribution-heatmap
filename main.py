eda74e379f2d4240a2dc5994613b57b5482e94e1
rivemont <william.11bennett@gmail.com>
2023-12-01 09:51:25 -0500

    Project outlining.

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ Will Bennett
 """
 
 # TODO parsing
+# separate ingest functions for each data source
+# each returns a data class that has: data, source name, date range, color scale
 
 # TODO combining
+# function for creating a combined data struct given 1 or more sources
 
 # TODO displaying
+# function for generating calendar svg given data
+# get current date
+# build shape of calendar (xyz)
+# create legend with colors of each source
 
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
